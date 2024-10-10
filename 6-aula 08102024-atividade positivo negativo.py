c2195bc1b6c833c041f6620a0a2341b46dbdace3
Ellisiane <106766886+ellisiane@users.noreply.github.com>
2024-10-10 14:55:12 -0300

    Add files via upload

---
+++ 6-aula 08102024-atividade positivo negativo.py
@@ -0,0 +1,8 @@
+# Entrada de dados
+valor = float(input("Digite um valor: "))
+
+# Verificação se é positivo ou negativo
+if valor >= 0:
+    print("O valor é positivo")
+else:
+    print("O valor é negativo")
